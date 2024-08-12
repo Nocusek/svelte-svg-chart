fde2e8e5811610fc6b7a2bf302feb4141090ee29
Mateusz Nocoń <mateusznoconxyz@gmail.com>
2024-08-12 21:47:00 +0200

    add polyline to graph

--- src/lib/LineGraph/line-graph.svelte
+++ src/lib/LineGraph/line-graph.svelte
@@ -1,25 +1,71 @@
 <script>
 
+    let graphWidth = 300;
+    let graphHeight = 200;
+
     let xPointsCount = 10;
+    // @ts-ignore
     let yPointsCount = 10;
+
+    // @ts-ignore
+    let points = [
+        {
+            x: 10,
+            y: 10
+        },
+        {
+            x: 50,
+            y: 50,
+        },
+        {
+            x: 80,
+            y: 30
+        }
+    ]
+
+    // @ts-ignore
+    function formatPoints(points) {
+        // @ts-ignore
+        return points.map(point => `${point.x},${point.y}`).join(' ');
+    }
 
 </script>
 
 <svg version="1.1"
-     width="300" height="200"
+     width={graphWidth} height={graphHeight}
      xmlns="http://www.w3.org/2000/svg">
+     
+     <g>
+        <line x1="0" x2="0" y1="0" y2="200" stroke="orange" stroke-width="5"/>
+        {#each Array(xPointsCount) as _, i}
+            <circle cx="0" cy={i * 20} r="5" stroke="red" fill="red"/>
+        {/each}
+     </g>
 
-    <line x1="0" x2="0" y1="0" y2="200" stroke="orange" stroke-width="5"/>
-    <line x1="0" x2="300" y1="200" y2="200" stroke="orange" stroke-width="5"/>
+     <g>
+        <line x1="0" x2="300" y1="200" y2="200" stroke="orange" stroke-width="5"/>
+        {#each Array(xPointsCount) as _, i}
+            <circle cx="{i * 20}" cy="200" r="5" stroke="red" fill="red"/>
+        {/each}
+     </g>
 
-
-    {#each Array(xPointsCount) as _, i}
-        <circle cx="0" cy={i * 20} r="5" stroke="red" fill="red"/>
-    {/each}
-
+     <g>
+        <polyline 
+        class="polyline"
+        points={formatPoints(points)}/>
+     </g> 
 
 </svg>
 
 <style>
+.polyline {
+    fill: none;
+    stroke: blue;
+    stroke-linejoin: round;
+    stroke-linecap: round;
 
+    &:hover {
+        stroke-width: 5;
+    }
+}
 </style>
